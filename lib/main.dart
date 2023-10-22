44d3fc609b28333168e37de9372c4987c7528107
felipe <felipeavmuller@gmail.com>
2023-10-21 22:08:19 -0300

    challenge

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,48 @@ void main() {
   runApp(const MyApp());
 }
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.blue[300],
+//         body: SafeArea(
+//             child: Column(
+//                 mainAxisSize: MainAxisSize.min, //Limita o tamanho da coluna
+//                 // mainAxisAlignment: MainAxisAlignment.spaceEvenly, tipo flex box
+//                 crossAxisAlignment: CrossAxisAlignment.stretch,
+//                 children: <Widget>[
+//               Container(
+//                 color: Colors.white,
+//                 height: 100.0,
+//                 width: 100,
+//                 child: const Text('Container 1'),
+//               ),
+//               const SizedBox(height: 10),
+//               Container(
+//                 color: Colors.teal,
+//                 height: 100.0,
+//                 width: 100,
+//                 child: const Text('Container 2'),
+//               ),
+//               const SizedBox(height: 10),
+//               Container(
+//                 color: Colors.red,
+//                 height: 100.0,
+//                 width: 100,
+//                 child: const Text('Container 3'),
+//               ),
+//               const SizedBox(height: 10),
+//             ])),
+//       ),
+//     );
+//   }
+// }
+
+// challenge:
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,18 +53,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue[300],
-        body: SafeArea(
-          child: Container(
-            color: Colors.teal,
-            child: Text('Hello World'),
-            height: 100.0,
-            width: 100,
-            margin: EdgeInsets.only(left: 30),
-            padding: EdgeInsets.all(20.0),
-          ),
-        ),
-      ),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.yellow,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 100,
+                      height: 100,
+                    )
+                  ],
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                )
+              ]))),
     );
   }
 }
